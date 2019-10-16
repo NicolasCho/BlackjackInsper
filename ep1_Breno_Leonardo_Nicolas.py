37f61c49d4bc9036cffc3f5570b8107a09296061
brenoeboli <49624509+brenoeboli@users.noreply.github.com>
2019-10-16 19:07:32 -0300

    Update ep1_Breno_Leonardo_Nicolas.py

--- ep1_Breno_Leonardo_Nicolas.py
+++ ep1_Breno_Leonardo_Nicolas.py
@@ -1,26 +1,21 @@
 import random
 
 
-def saque(baralho):
-    random.shuffle(baralho)
-    carta = baralho.pop()
-    return carta
-
-
-def soma(mao):
+def contador(mao):
     a = False
     listasemas = []
     i = 0
     while i < len(mao):
-        if mao[i] != 1:
-            listasemas.append(mao[i])
+        if mao[i][1] != 1:
+            listasemas.append(mao[i][1])
             i += 1
-        elif mao[i] == 1 and '1' not in listasemas:
+        elif mao[i][1] == 1 and '1' not in listasemas:
             a = True
             for e in mao[i + 1:]:
-                if mao[i] == e:
-                    listasemas.append(e)
+                if mao[i][1] == e[1]:
+                    listasemas.append(e[1])
             i += 1
+
     somatoriosemas = 0
     for g in listasemas:
         somatoriosemas = somatoriosemas + g
@@ -33,238 +28,212 @@ def soma(mao):
     return soma
 
 
-def maior_soma(lista_jogadores, dic_valores):
-    somatorias_jogadores = []
-    for e in lista_jogadores:
-        somatorias_jogadores.append(soma(dic_valores[e]))
-    return max(somatorias_jogadores)
+quantidade_de_jogadores = int(input('Insira a quantidade de jogadores: '))
+lista_jogadores = []
+j = 0
 
+while j < quantidade_de_jogadores:
+    x = input('Jogador {}, insira seu nick: '.format(j + 1))
+    lista_jogadores.append(x)
+    j += 1
 
-def valor(carta):
-    a = carta[1]
-    if a == 'J' or a == 'Q' or a == 'K':
-        a = 10
-    elif a == 'As':
-        a = 1
-    else:
-        a = int(carta[1])
-    return a
+valores_carteiras = []
+z = 0
+while z < len(lista_jogadores):
+    valor = float(input("Insira o valor da sua carteira {}: ".format(lista_jogadores[z])))
+    valores_carteiras.append(valor)
+    z += 1
 
-    # FEATURE 4: MULTIJOGADOR!
+saldo = []
 
+for carteira in valores_carteiras:
+    while carteira > 0:
+        v = False
+        n = False
+        x = True
+        y = True
 
-n = input('Quantas pessoas vao jogar? ')
-nv = n.isdigit()
-while nv is False:
-    print('Você não digitou um número inteiro! Digite um número válido dessa vez.')
-    n = input('Quantas pessoas vao jogar? ')
-    nv = n.isdigit()
-n = int(n)
+        c = 0
+        lista_apostas = []
+        while c < len(lista_jogadores):
+            aposta = float(input('Insira o valor da sua aposta, {}: '.format(lista_jogadores[c])))
+            while aposta > carteira:
+                print('Você não tem esse tanto de dinheiro! Faça uma aposta possível')
+                aposta = float(input('Insira o valor da sua aposta, {}: '.format(lista_jogadores[c])))
+            lista_apostas.append(aposta)
+            saldo.append(valores_carteiras[c] - lista_apostas[c])
+            c += 1
 
-lista_jogadores_definitiva = []
-lista_carteira = []
-dic_carteira = {}
+        cartas = {
+            'Ás de  paus (♣)': 1,
+            '2 de paus (♣)': 2,
+            '3 de paus (♣)': 3,
+            '4 de paus (♣)': 4,
+            '5 de paus (♣)': 5,
+            '6 de paus (♣)': 6,
+            '7 de paus (♣)': 7,
+            '8 de paus (♣)': 8,
+            '9 de paus (♣)': 9,
+            '10 de paus (♣)': 10,
+            'Q de paus (♣)': 10,
+            'J de paus (♣)': 10,
+            'K de paus (♣)': 10,
 
-i = 0
-while i < n:
-    nome = str(input('Nome do jogador: '))
-    carteira = input('{}, quanto reais você possui?'.format(nome))
-    carteirav = carteira.isdigit()
-    while carteirav is False:
-        print('Você não digitou um número inteiro! Digite um número válido dessa vez.')
-        carteira = input('Quantas pessoas vao jogar? ')
-        carteirav = carteira.isdigit()
-    carteira = int(carteira)
-    lista_jogadores_definitiva.append(nome)
-    dic_carteira[lista_jogadores_definitiva[i]] = carteira
-    i += 1
+            'Ás de copas (♥)': 1,
+            '2 de copas (♥)': 2,
+            '3 de copas (♥)': 3,
+            '4 de copas (♥)': 4,
+            '5 de copas (♥)': 5,
+            '6 de copas (♥)': 6,
+            '7 de copas (♥)': 7,
+            '8 de copas (♥)': 8,
+            '9 de copas (♥)': 9,
+            '10 de copas (♥)': 10,
+            'Q de copas (♥)': 10,
+            'J de copas (♥)': 10,
+            'K de copas (♥)': 10,
 
-    # FEATURE 5:MULTIPLOS BARALHOS!
-numero_de_baralhos = input('Quantos baralhos? ')
-numero_de_baralhosv = numero_de_baralhos.isdigit()
-while numero_de_baralhosv is False:
-    print('Você não digitou um número inteiro! Digite um número válido dessa vez.')
-    numero_de_baralhos = input('Quantos baralhos? ')
-    numero_de_baralhosv = numero_de_baralhos.isdigit()
-numero_de_baralhos = int(numero_de_baralhos)
+            'Ás de espadas (♠))': 1,
+            '2 de espadas (♠)': 2,
+            '3 de espadas (♠)': 3,
+            '4 de espadas (♠)': 4,
+            '5 de espadas (♠)': 5,
+            '6 de espadas (♠)': 6,
+            '7 de espadas (♠)': 7,
+            '8 de espadas (♠)': 8,
+            '9 de espadas (♠)': 9,
+            '10 de espadas (♠)': 10,
+            'Q de espadas (♠)': 10,
+            'J de espadas (♠)': 10,
+            'K de espadas (♠)': 10,
 
-numeros = ['As', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * numero_de_baralhos
-naipes = ['Espadas ♠', 'Copas ♥', 'Paus ♣', 'Ouros ♦'] * numero_de_baralhos
-baralho = [[p + ' de ' + n, p] for n in naipes for p in numeros]
+            'Ás de ouros (♦)': 1,
+            '2 de ouros (♦)': 2,
+            '3 de ouros (♦)': 3,
+            '4 de ouros (♦)': 4,
+            '5 de ouros (♦)': 5,
+            '6 de ouros (♦)': 6,
+            '7 de ouros (♦)': 7,
+            '8 de ouros (♦)': 8,
+            '9 de ouros (♦)': 9,
+            '10 de ouros (♦)': 10,
+            'Q de ouros (♦)': 10,
+            'J de ouros (♦)': 10,
+            'K de ouros (♦)': 10
+        }
+        soma = 0
+        mao_dealer = []
+        valores_soma = []
+        mao = []
 
-RESTAURAR = False
-JOGAR = True
+        for b in range(len(lista_jogadores)):
+            PRIMEIRA_CARTA = random.choice(list(cartas.items()))
+            del (cartas[PRIMEIRA_CARTA[0]])
+            SEGUNDA_CARTA = random.choice(list(cartas.items()))
+            del (cartas[SEGUNDA_CARTA[0]])
+            CARTA_DEALER_CIMA = random.choice(list(cartas.items()))
+            del (cartas[CARTA_DEALER_CIMA[0]])
+            mao.append(PRIMEIRA_CARTA)
+            mao.append(SEGUNDA_CARTA)
+            print('{}, você retirou {} e {}. Sua soma é {}.'.format(lista_jogadores[b], PRIMEIRA_CARTA[0],
+                                                                    SEGUNDA_CARTA[0], contador(mao)))
+            valores_soma.append(contador(mao))
 
-while JOGAR:
-    lista_jogadores = []
-    dic_cartas = {}
-    dic_valores = {}
-    dic_apostas = {}
-    valor_dealer = []
-    cartas_dealer = []
-    for e in lista_jogadores_definitiva:
-        lista_jogadores.append(e)
+        CARTA_DEALER_BAIXO = random.choice(list(cartas.items()))
+        del (cartas[CARTA_DEALER_BAIXO[0]])
+        CARTA_DEALER_CIMA = random.choice(list(cartas.items()))
+        del (cartas[CARTA_DEALER_CIMA[0]])
+        mao_dealer.append(CARTA_DEALER_CIMA)
+        mao_dealer.append(CARTA_DEALER_BAIXO)
+        print('A carta virada para cima do dealer é {}'.format(CARTA_DEALER_CIMA[0]))
 
-    # FEATURE LIVRE: RESTAURAR BARALHO OU MANTER
-    if RESTAURAR == True:
-        novo_baralho = str(input('Digite [1] para restaurar o baralho e [2] para manter: '))
-        while novo_baralho != '1' and '2':
-            novo_baralho = str(input('Digite [1] para restaurar o baralho e [2] para manter: '))
-
-        if novo_baralho == '1':
-            numero_de_baralhos = input('Quantos baralhos? ')
-            numero_de_baralhosv = numero_de_baralhos.isdigit()
-            while numero_de_baralhosv is False:
-                print('Você não digitou um número inteiro! Digite um número válido dessa vez.')
-                numero_de_baralhos = input('Quantas pessoas vao jogar? ')
-                numero_de_baralhosv = numero_de_baralhos.isdigit()
-            numero_de_baralhos = int(numero_de_baralhos)
-
-            numeros = ['As', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * numero_de_baralhos
-            naipes = ['Espadas ♠', 'Copas ♥', 'Paus ♣', 'Ouros ♦'] * numero_de_baralhos
-            baralho = [[p + ' de ' + n, p] for n in naipes for p in numeros]
-
-        # FEATURES 1 E 2: LIMITE DE APOSTA E FIM DO JOGO
-    e = 0
-    while e < len(lista_jogadores):
-        aposta = str(input('{}, quanto você quer apostar? Digite [fim] para sair do jogo: '.format(lista_jogadores[e])))
-        if aposta == 'fim':
-            del (dic_carteira[lista_jogadores[e]])
-            del (lista_jogadores[e])
-            del (lista_jogadores_definitiva[e])
-            e -= 1
-        else:
-            aposta = float(aposta)
-            while aposta > dic_carteira[lista_jogadores[e]] or aposta <= 0:
-                aposta = float(input('Aposta impossível! Digite um valor possível: '))
-            dic_apostas[lista_jogadores[e]] = aposta
-        e += 1
-
-    if len(lista_jogadores) < 1:
-        break
-
-    i = 0
-    while i < len(lista_jogadores):
-        carta1 = saque(baralho)
-        carta2 = saque(baralho)
-        lista_valores = [valor(carta1), valor(carta2)]
-        lista_cartas = [carta1[0], carta2[0]]
-        dic_valores[lista_jogadores[i]] = lista_valores
-        dic_cartas[lista_jogadores[i]] = lista_cartas
-
-        if soma(dic_valores[lista_jogadores[i]]) == 21:
-            print('BLACKJACK!.{}, você fez 21'.format(lista_jogadores[i]))
-            dic_carteira[lista_jogadores[i]] = dic_carteira[lista_jogadores[i]] + dic_apostas[lista_jogadores[i]] * 1.5
-            del (lista_jogadores[i])
-            i -= 1
-        i += 1
-
-    print('Suas mãos:{}.'.format(dic_cartas))
-
-    # FEATURE LIVRE:CARTA VIRADA PARA CIMA DO DEALER
-    baixo_dealer = saque(baralho)
-    cima_dealer = saque(baralho)
-    valor_dealer.append(valor(baixo_dealer))
-    valor_dealer.append(valor(cima_dealer))
-    cartas_dealer.append(baixo_dealer[0])
-    cartas_dealer.append(cima_dealer[0])
-    print('Carta de cima do dealer:{}'.format(cima_dealer[0]))
-
-    i = 0
-    while i < len(lista_jogadores):
-        continuidade = str(input('{}, digite [1] para pedir mais cartas ou [2] para parar:'.format(lista_jogadores[i])))
-        while continuidade != "1" and continuidade != '2':
-            print("Você não digitou um valor válido!")
-            continuidade = str(input('{}, digite [1] para pedir mais cartas ou [2] para parar:'.format(lista_jogadores[i])))
-
-        while continuidade == '1':
-            carta_extra = saque(baralho)
-            dic_valores[lista_jogadores[i]].append(valor(carta_extra))
-            dic_cartas[lista_jogadores[i]].append(carta_extra[0])
-            print('{}, sua nova mão: {}'.format(lista_jogadores[i], dic_cartas[lista_jogadores[i]]))
-
-            if soma(dic_valores[lista_jogadores[i]]) > 21:
-                print('{}, você ESTOUROU!'.format(lista_jogadores[i]))
-                dic_carteira[lista_jogadores[i]] = dic_carteira[lista_jogadores[i]] - dic_apostas[lista_jogadores[i]]
-                del (lista_jogadores[i])
-                i -= 1
-                break
-            elif soma(dic_valores[lista_jogadores[i]]) == 21:
-                print('BLACKJACK!.{}, você fez 21'.format(lista_jogadores[i]))
-                dic_carteira[lista_jogadores[i]] = dic_carteira[lista_jogadores[i]] + dic_apostas[
-                    lista_jogadores[i]] * 1.5
-                del (lista_jogadores[i])
-                i -= 1
-                break
+        lista_verifica_saida = []
+        for t1 in range(len(lista_jogadores)):
+            if valores_soma[t1] == 21:
+                print('Blackjack! {}, você fez 21!'.format(lista_jogadores[t1]))
+                saldo[t1] = saldo[t1] + 1.5 * lista_apostas[c]
+                print('{}, seu novo saldo é de R${}.'.format(lista_jogadores[t1], saldo[t1]))
+                lista_verifica_saida.append('hold')
             else:
-                continuidade = str(
-                    input('{}, digite [1] para pedir mais cartas ou [2] para parar:'.format(lista_jogadores[i])))
-        i += 1
+                lista_verifica_saida.append('hit')
 
-    if len(lista_jogadores) > 0:
-        print('Mão do dealer era: {}'.format(cartas_dealer))
-        while soma(valor_dealer) < 17:
-            carta_extra_dealer = saque(baralho)
-            valor_dealer.append(valor(carta_extra_dealer))
-            cartas_dealer.append(carta_extra_dealer[0])
-        while soma(valor_dealer) < maior_soma(lista_jogadores, dic_valores):
-            carta_extra_dealer2 = saque(baralho)
-            valor_dealer.append(valor(carta_extra_dealer2))
-            cartas_dealer.append(carta_extra_dealer2[0])
-        print('Após puxar cartas, a nova mão do dealer: {}. A soma do dealer foi {}'.format(cartas_dealer,
-                                                                                            soma(valor_dealer)))
+        for t2 in range(len(lista_jogadores)):
+            if lista_verifica_saida[t2] == 'hold':
+                print('{}, você não está mais no jogo.'.format(lista_jogadores[t2]))
+                valores_soma[t2] = valores_soma[t2]
 
-        for e in lista_jogadores:
-            if soma(valor_dealer) > 21:
-                print('{}, você GANHOU!'.format(e))
-            elif soma(dic_valores[e]) < soma(valor_dealer) and soma(valor_dealer) <= 21:
-                print('{}, você PERDEU!'.format(e))
-                dic_carteira[e] = dic_carteira[e] - dic_apostas[e]
-            elif soma(dic_valores[e]) == soma(valor_dealer):
-                print('{}, você EMPATOU!'.format(e))
-
-    print('RESULTADOS')
-    print('MÃOS DOS JOGADORES:{}'.format(dic_cartas))
-    print('MÃO DO DEALER:{}'.format(cartas_dealer))
-    print('CARTEIRAS:{}'.format(dic_carteira))
-
-    # FEATURE LIVRE: SEGUNDA CHANCE!
-    e = 0
-    while e < len(lista_jogadores_definitiva):
-        if dic_carteira[lista_jogadores_definitiva[e]] <= 0:
-            print('{}, acabou deu dinheiro! Algum jogador deseja dar dinheiro para {}?'.format(
-                lista_jogadores_definitiva[e], lista_jogadores_definitiva[e]))
-            doar = str(input('Se sim DIGITE O NOME DO JOGADOR. Se não digite [nao]:'))
-            if doar == 'nao':
-                print('{}, mais sorte na proxima!'.format(lista_jogadores_definitiva[e]))
-                del (dic_carteira[lista_jogadores_definitiva[e]])
-                del (lista_jogadores[e])
-                del (lista_jogadores_definitiva[e])
-                e -= 1
             else:
-                doacao = input('{}, quanto você deseja doar:'.format(doar))
-                doacaov = doacao.isdigit()
-                while doacaov is False:
-                    print('Você não digitou um número válido! Digite um número válido dessa vez.')
-                    doacao = input('Quanto você deseja doar? ')
-                    doacaov = doacao.isdigit()
-                doacao = float(doacao)
-                
-                dic_carteira[lista_jogadores_definitiva[e]] = doacao
-                dic_carteira[doar] = dic_carteira[doar] - doacao
-            print('CARTEIRAS:{}'.format(dic_carteira))
-        e += 1
+                print('Vez de {}'.format(lista_jogadores[t2]))
+                mais_carta = str(
+                    input('{}, digite [hit] para mais cartas ou [hold] para fechar a mão:'.format(lista_jogadores[t2])))
+                lista_verifica_saida[t2] = mais_carta
 
-    RESTAURAR = True
+                while mais_carta == 'hit':
+                    v = True
+                    CARTA_EXTRA = random.choice(list(cartas.items()))
+                    del (cartas[CARTA_EXTRA[0]])
+                    mao.clear()
+                    valores_soma[t2] = valores_soma[t2] + CARTA_EXTRA
+                    mao.append(valores_soma[t2])
+                    print('Você retirou {}. Sua mão é {}. A soma é {}.'.format(CARTA_EXTRA[0], mao, contador(mao)))
 
-    for e in lista_jogadores_definitiva:
-        del (dic_apostas[e])
+                    if valores_soma[t2] > 21:
+                        print('{}, você estorou!'.format(lista_jogadores[t2]))
+                        saldo[t2] = saldo[t2]
+                        print('{}, seu novo saldo é de R${}.'.format(lista_jogadores[t2], carteira))
+                        lista_verifica_saida[t2] = 'hold'
 
-    if len(lista_jogadores_definitiva) < 1:
-        JOGAR = False
+                    elif valores_soma[t2] == 21:
+                        print('{}, você fez 21!'.format(lista_jogadores[t2]))
+                        saldo[t2] = saldo[t2] + 1.5 * lista_apostas[t2]
+                        print('{}, seu novo saldo é de R${}.'.format(lista_jogadores[t2], saldo[t2]))
+                        lista_verifica_saida[t2] = 'hold'
 
-    continuar = str(input('Digite [1] para continuar a jogar e [2] para sair:'))
-    if continuar == '2':
-        JOGAR = False
+                    else:
+                        mais_carta = str(input('Digite [hit] para mais cartas ou [hold] para fechar a mão:'))
+                        lista_verifica_saida[t2] = mais_carta
 
-print('Até a próxima!')
+                    if lista_verifica_saida[0:] == 'hold':
+                        menor = valores_soma[0]
+                        for elemento1 in valores_soma:
+                            if elemento1 < menor:
+                                menor = elemento1
+
+                        n = True
+                        print('A carta virada para baixo do dealer é {}.A soma do dealer é {}'.format(
+                            CARTA_DEALER_BAIXO[0],
+                            contador(mao_dealer)))
+                        while contador(mao_dealer) < 17:
+                            CARTA_DEALER_EXTRA = random.choice(list(cartas.items()))
+                            del (cartas[CARTA_DEALER_EXTRA[0]])
+                            mao_dealer.append(CARTA_DEALER_EXTRA)
+                        while contador(mao_dealer) < menor:
+                            CARTA_DEALER_EXTRA2 = random.choice(list(cartas.items()))
+                            del (cartas[CARTA_DEALER_EXTRA2[0]])
+                            mao_dealer.append(CARTA_DEALER_EXTRA2)
+
+                        print('O dealer puxou as cartas. A nova mão do dealer é {}. A soma do dealer é {}'.format(
+                            mao_dealer,
+                            contador(
+                                mao_dealer)))
+
+                        # mostar os resultados
+
+                        for u in range(len(lista_jogadores)):
+                            if valores_soma[u] < 21:
+                                if contador(mao_dealer) > 21:
+                                    print('{}, você GANHOU!')
+                                    saldo[u] = saldo[u] + 1.5 * lista_apostas[u]
+                                    print('{}, seu novo saldo é de R${}.'.format(lista_jogadores[u], saldo[u]))
+                                elif contador(mao_dealer) > contador(mao):
+                                    print('Você PERDEU!')
+                                    saldo[t2] = saldo[t2]
+                                    print('Seu novo saldo é de R${}.'.format(saldo[u]))
+                                elif contador(mao_dealer) == contador(mao):
+                                    print('EMPATE!')
+                                    saldo[t2] = saldo[t2] + lista_jogadores[u]
+                                    print('Seu novo saldo é de R${}.'.format(saldo[u]))
+
+# plotar tabela com valores --> Resultados:
+# Nome / Carteira / Aposta/ Soma das Cartas / Saldo
+# Perguntar quem quer jogar novamente --> fazer lista de saída
